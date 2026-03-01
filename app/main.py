a73b02b6c6e033070a144263f0c122c6d4f8c984
Zubia Batool <zubiabatool4@gmail.com>
2026-03-01 21:09:14 +0500

    add unit testing using Testclient in pychart

--- app/main.py
+++ app/main.py
@@ -4,10 +4,22 @@ from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.util import get_remote_address
 from slowapi.errors import RateLimitExceeded
 from app.database import database_engine, Base
-
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI(
     title="URL shortner", description=" A simple API shortner with analytics tracking"
 )
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# setting up rate limiter
+limiter = Limiter(key_func=get_remote_address)
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 # creating database table
 Base.metadata.create_all(bind=database_engine)
@@ -16,10 +28,7 @@ Base.metadata.create_all(bind=database_engine)
 app.include_router(url.router)
 app.include_router(analytics.router)
 
-# setting up rate limiter
-limiter = Limiter(key_func=get_remote_address)
-app.state.limiter = limiter
-app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
+
 
 
 # root
---
+++ tests/test_urls.py
@@ -0,0 +1,44 @@
+from fastapi.testclient import TestClient
+from app.main import app
+
+client = TestClient(app)
+
+def test_create_short_url():
+    response = client.post("/shorten", json={
+        "original_url": "https://google.com",
+    
+        "short_code": "test999"
+    })
+    assert response.status_code == 201
+
+def test_create_short_url1():
+    response = client.post("/shorten", json={
+        "original_url": "https://youtube.com",
+        "expiry_date": "2024-01-01T00:00:00",
+        
+    })
+    assert response.status_code == 201
+
+def test_create_short_url_2():
+    response = client.post(
+        "/shorten", json = {
+            "original_url" : "https://github.com",
+            "expiry_date": "2024-03-01T08:15:00",
+            "short_code" : "git"
+        }
+    )
+
+def test_expired_redirect():
+    # 1. Create the short URL
+    response = client.post("/shorten", json={
+        "original_url": "https://youtube.com",
+        "expiry_date": "2020-01-01T00:00:00",  # past date to simulate expired link
+    })
+    assert response.status_code == 201
+
+    short_code = response.json()["short_code"]
+
+    # 2. Try to access the short URL
+    redirect_response = client.get(f"/{short_code}")
+    assert redirect_response.status_code == 410  # now this should pass
+
